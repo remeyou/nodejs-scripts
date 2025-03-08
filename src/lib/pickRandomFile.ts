896c51114fa1aef698ff1efbdb56297448574321
Reme You <73418960+remeyou@users.noreply.github.com>
2025-03-08 22:17:56 +0800

    feat: add cache expired time

--- src/lib/pickRandomFile.ts
+++ src/lib/pickRandomFile.ts
@@ -1,75 +1,75 @@
-import { execFile } from 'child_process'
-import { mkdir, readFile, readdir, stat, writeFile } from 'fs/promises'
-import { parse, resolve } from 'path'
-import { askFiletype, askPath } from '../utils'
+import { execFile } from "child_process";
+import { mkdir, readFile, readdir, stat, writeFile } from "fs/promises";
+import { parse, resolve } from "path";
+import { askFiletype, askPath } from "../utils";
 
 const generateCache = async (path: string, cachePath: string) => {
-  console.log('Generating cache, it will take for a moment...')
-  const list = await readdir(path, { recursive: true })
+  console.log("Generating cache, it will take for a moment...");
+  const list = await readdir(path, { recursive: true });
   writeFile(cachePath, JSON.stringify({ timestamp: Date.now(), path, list }), {
-    encoding: 'utf-8',
-  })
-  return list
-}
+    encoding: "utf-8",
+  });
+  return list;
+};
 
 const readCache = async (path: string, tmpPath: string) => {
-  const cachePath = tmpPath + '\\' + parse(path).name + '.txt'
+  const cachePath = tmpPath + "\\" + parse(path).name + ".txt";
   try {
-    await stat(cachePath)
-    const cache = JSON.parse(await readFile(cachePath, { encoding: 'utf-8' }))
+    await stat(cachePath);
+    const cache = JSON.parse(await readFile(cachePath, { encoding: "utf-8" }));
+    const cacheExpiredDays =
+      (Number(process.argv[5]) || 7) * 24 * 60 * 60 * 1000;
     if (
       path === cache.path &&
-      Date.now() - cache.timestamp < 24 * 60 * 60 * 1000
+      Date.now() - cache.timestamp < cacheExpiredDays
     ) {
-      return cache.list as string[]
+      return cache.list as string[];
     }
   } catch (error) {}
-  return generateCache(path, cachePath)
-}
+  return generateCache(path, cachePath);
+};
 
 const init = async (path: string) => {
-  const tmpPath = __dirname + '\\tmp'
+  const tmpPath = __dirname + "\\tmp";
   try {
-    await stat(tmpPath)
+    await stat(tmpPath);
   } catch (error) {
-    await mkdir(tmpPath, { recursive: true })
+    await mkdir(tmpPath, { recursive: true });
   }
-
-  return readCache(path, tmpPath)
-}
+  return readCache(path, tmpPath);
+};
 
 const pickRandomFile = async () => {
-  const path = process.argv[3] || (await askPath())
-  const acceptFiletype = process.argv[4] || (await askFiletype()).join(',')
-
-  const list = await init(path)
-  let idx = Math.floor(Math.random() * list.length)
-  const initIdx = idx
+  const path = process.argv[3] || (await askPath());
+  const acceptFiletype = process.argv[4] || (await askFiletype()).join(",");
+  const list = await init(path);
+  let idx = Math.floor(Math.random() * list.length);
+  const initIdx = idx;
 
   while (true) {
-    const relativePath = list[idx]
-    const absolutePath = resolve(path, relativePath)
-    const { ext } = parse(absolutePath)
+    const relativePath = list[idx];
+    const absolutePath = resolve(path, relativePath);
+    const { ext } = parse(absolutePath);
 
     if (ext && (!acceptFiletype || acceptFiletype.includes(ext.slice(1)))) {
       try {
-        await stat(absolutePath)
-        console.log('[OPEN]', absolutePath)
-        execFile(`explorer`, [absolutePath])
-        break
+        await stat(absolutePath);
+        console.log("[OPEN]", absolutePath);
+        execFile(`explorer`, [absolutePath]);
+        break;
       } catch (error) {
-        console.log('[NO EXIST]', absolutePath)
+        console.log("[NO EXIST]", absolutePath);
       }
     } else {
-      console.log('[SKIP]', absolutePath)
+      console.log("[SKIP]", absolutePath);
     }
 
-    idx = idx === list.length - 1 ? 0 : idx + 1
+    idx = idx === list.length - 1 ? 0 : idx + 1;
     if (idx === initIdx) {
-      console.log('Cannot find a proper file.')
-      break
+      console.log("Cannot find a proper file.");
+      break;
     }
   }
-}
+};
 
-export default pickRandomFile
+export default pickRandomFile;
